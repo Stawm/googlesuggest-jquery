7fb69a5e1ccd93f6da9ed0eff8852ac7f7bb9b24
haochi.chen <haochi.chen@7943ea2a-0d1d-75e0-5d18-4f893e787f62>
2010-11-06 18:33:16 +0000

    Initial commit.

---
+++ trunk/jquery.googleSuggest.js
@@ -0,0 +1,28 @@
+/**@license
+This file uses Google Suggest for jQuery plugin (licensed under GPLv3) by Haochi Chen ( http://ihaochi.com )
+ */
+$.fn.googleSuggest = function(opts){
+  var services = {youtube: 'yt', books: 'bo', products: 'pr', news: 'n'};
+  
+  opts = $.extend({service: '', secure: false}, opts);
+  opts.source = function(request, response){
+    $.ajax({
+      url: 'http'+(opts.secure?'s':'')+'://clients1.google.com/complete/search',
+      dataType: 'jsonp',
+      data: {
+        q: request.term,
+        ds: opts.service in services ? services[opts.service] : '',
+        nolabels: 't'
+      },
+      success: function( data ) {
+        response($.map(data[1], function(item){
+          return { value: item[0] }
+        }));
+      }
+    });  
+  };
+  
+  return this.each(function(){
+    $(this).autocomplete(opts);
+  });
+}
