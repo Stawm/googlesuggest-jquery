eb24158894b5dfba3ea4b97d83a3fef7911e8e97
haochi.chen <haochi.chen@gmail.com>
2011-12-03 16:20:16 +0000

    Fix after Google update to use the client parameter instead of the ds param

--- jquery.googleSuggest.js
+++ jquery.googleSuggest.js
@@ -2,21 +2,21 @@
 This file uses Google Suggest for jQuery plugin (licensed under GPLv3) by Haochi Chen ( http://ihaochi.com )
  */
 $.fn.googleSuggest = function(opts){
-  var services = {youtube: 'yt', books: 'bo', products: 'pr', news: 'n', images: 'i'};
-  
-  opts = $.extend({service: '', secure: false}, opts);
+  var services = {youtube: 'youtube', books: 'books', products: 'products', news: 'news', images: 'img', web: 'psy'};
+
+  opts = $.extend({service: 'web', secure: false}, opts);
   opts.source = function(request, response){
     $.ajax({
       url: 'http'+(opts.secure?'s':'')+'://clients1.google.com/complete/search',
       dataType: 'jsonp',
       data: {
         q: request.term,
-        ds: opts.service in services ? services[opts.service] : '',
+        client: services[opts.service],
         nolabels: 't'
       },
-      success: function( data ) {
+      success: function(data) {
         response($.map(data[1], function(item){
-          return { value: item[0] }
+          return { value: $("<span>").html(item[0]).text() }
         }));
       }
     });  
